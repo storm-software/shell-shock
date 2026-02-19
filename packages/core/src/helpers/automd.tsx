319919f8bb62e78256241ae158c63643187166a5
Pat <pat@patsullivan.org>
2026-02-19 05:40:15 +0000

    fix(core): Resolve issue with missing typescript import

--- packages/core/src/helpers/automd.tsx
+++ packages/core/src/helpers/automd.tsx
@@ -24,6 +24,7 @@ import { defineGenerator } from "automd";
 import { CommandDocs } from "../components/docs";
 import { getAppTitle } from "../plugin-utils";
 import type { Context } from "../types/context";
+import { Spacing } from "@powerlines/plugin-alloy/core/components/spacing";
 
 /**
  * AutoMD generator to generate CLI command documentation
